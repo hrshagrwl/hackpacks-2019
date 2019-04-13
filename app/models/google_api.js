4c1233da89d90fe9e439add20d4f18184e36cd57
Suraj <ssuraj.here@gmail.com>
2019-04-13 17:15:13 -0400

    Bucket Upload

--- app/models/google_api.js
+++ app/models/google_api.js
@@ -1,6 +1,10 @@
 const async = require('async')
+const gcsHelpers = require('../../helpers/google-cloud-storage');
+const { storage } = gcsHelpers;
 
-async function upload_file() {
+const DEFAULT_BUCKET_NAME = 'hackpack-2019';
+
+async function get_transcript(audio_url) {
     // Imports the Google Cloud client library
     const speech = require('@google-cloud/speech');
     const fs = require('fs');
@@ -8,16 +12,16 @@ async function upload_file() {
     // Creates a client
     const client = new speech.SpeechClient();
   
-    // The name of the audio file to transcribe
-    const fileName = './resources/5- Minute Lecture Professor Irwin Goldman.mp3';
+    // // The name of the audio file to transcribe
+    // const fileName = './resources/5- Minute Lecture Professor Irwin Goldman.mp3';
   
-    // Reads a local audio file and converts it to base64
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString('base64');
+    // // Reads a local audio file and converts it to base64
+    // const file = fs.readFileSync(fileName);
+    // const audioBytes = file.toString('base64');
   
     // The audio file's encoding, sample rate in hertz, and BCP-47 language code
     const audio = {
-      content: audioBytes,
+      uri: audio_url
     };
     const config = {
       encoding: 'LINEAR16',
@@ -36,4 +40,42 @@ async function upload_file() {
       .join('\n');
     console.log(`Transcription: ${transcription}`);
   }
-  exports.upload_file  = upload_file;
+  
+  /**
+   * Middleware for uploading file to GCS.
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * @return {*}
+   */
+  exports.sendUploadToGCS = (req, res, next) => {
+    if (!req.file) {
+      return next();
+    }
+    const bucketName = req.body.bucketName || DEFAULT_BUCKET_NAME;
+    const bucket = storage.bucket(bucketName);
+    const gcsFileName = `${Date.now()}-${req.file.originalname}`;
+    const file = bucket.file(gcsFileName);
+    const stream = file.createWriteStream({
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    });
+    stream.on('error', (err) => {
+      req.file.cloudStorageError = err;
+      next(err);
+    });
+    stream.on('finish', () => {
+      req.file.cloudStorageObject = gcsFileName;
+      return file.makePublic()
+        .then(() => {
+          req.file.gcsUrl = gcsHelpers.getPublicUrl(bucketName, gcsFileName);
+          next();
+        });
+    });
+    stream.end(req.file.buffer);
+	
+  };
+
+
+  exports.get_transcript  = get_transcript;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const async = require('async')
 const Google_api = require('./app/models/google_api')
+const Multer = require('multer');
 
 const app = express();
 
@@ -12,10 +13,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // router.route(app);
 
 const port = 8000;
+const upload = Multer({ dest: 'uploads/' });
+
+app.post(
+  '/upload',
+  upload.single('file'),
+  Google_api.sendUploadToGCS,
+  (req, res, next) => {
+    if (req.file && req.file.gcsUrl) {
+      // return res.send(req.file.gcsUrl);
+      console.log("req.file.gcsUrl");
+      Google_api.get_transcript(req.file.gcsUrl);
+    }
+    return res.status(500).send('Unable to upload');
+  },
+);
 
 app.listen(port, () => {
   console.log("App is listening on port : ", port);
-  Google_api.upload_file();
+  //Google_api.get_transcript();
 });
 
 module.exports = app
